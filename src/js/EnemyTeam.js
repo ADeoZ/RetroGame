059fe83941a4f39ac7f328147c173971f9366593
Aleksey Denisov <deoz@yandex.ru>
2021-04-21 01:28:40 +0300

    EnemyTeam step functions

--- src/js/EnemyTeam.js
+++ src/js/EnemyTeam.js
@@ -7,13 +7,14 @@ export default class EnemyTeam extends Team {
   constructor() {
     super();
     this.allowedTypes = [new Daemon(), new Undead(), new Vampire()];
-    this.startLines = [6, 7];
+    this.startLines = [3, 4];
   }
 
   turn(playerPositioned) {
     if (this.attack(playerPositioned)) {
       return this.attack(playerPositioned);
     }
+    // this.step(playerPositioned);
     return null;
   }
 
@@ -38,5 +39,55 @@ export default class EnemyTeam extends Team {
 
     const bestAttack = [].concat(...canAttack).sort((a, b) => a.coef - b.coef);
     return bestAttack[0];
+  }
+
+  step(playerPositioned) {
+    const boardSize = 8;
+    const distances = [];
+    this.positioned.forEach((member) => {
+      playerPositioned.forEach((character) => {
+        distances.push({
+          member,
+          targetIndex: character.position,
+          distance: EnemyTeam.calcSteps(member, character, boardSize),
+        });
+      });
+    });
+
+    distances.sort((a, b) => {
+      if (a.distance < b.distance) return -1;
+      if (a.distance > b.distance) return 1;
+      // if distance equal
+      if (a.member.character.attack > b.member.character.attack) return -1;
+      if (a.member.character.attack < b.member.character.attack) return 1;
+      return 0;
+    });
+
+    return null;
+  }
+
+  static calcSteps(index, target, boardSize) {
+    const vertical = Math.abs(
+      Math.floor(index.position / boardSize) - Math.floor(target.position / boardSize),
+    );
+    const horizontal = Math.abs(
+      Math.floor(index.position % boardSize) - Math.floor(target.position % boardSize),
+    );
+    const vertStep = vertical - index.character.attackRadius > 0
+      ? vertical - index.character.attackRadius : 0;
+
+    let horStep = 0;
+    if (vertStep === 0) {
+      horStep = horizontal - index.character.attackRadius;
+    } else {
+      horStep = horizontal - vertical - index.character.attackRadius + 1;
+    }
+    horStep = horStep > 0 ? horStep : 0;
+
+    // return vertical + diag - index.character.attackRadius >= 0
+    //   ? vertical + diag - index.character.attackRadius : 0;
+
+    return Math.ceil(vertStep / index.character.stepRadius)
+     + Math.ceil(horStep / index.character.stepRadius);
   }
 }
